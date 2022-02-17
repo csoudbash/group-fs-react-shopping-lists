ceebe47c1e5a8629f8906f2908c62f9dc916feae
Cameron Soudbash <csoudbash@gmail.com>
2022-02-17 14:16:30 -0600

    get route finished

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.get('/',(req,res) => {
+    const sqlText = `SELECT * FROM items ORDER BY name DESC;`;
+    pool.query(sqlText)
+    .then((result) => {
+        console.log('got da stuff from the DB', result);
+    }).catch((error) => {
+        console.log(`rut ro raggy, ${sqlText}`, error);
+    })
+})
 
 module.exports = router;
